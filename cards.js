77ddd4a8b312f1eb482070640b0a1c6216e4e69c
philltan916-create <philltan916@gmail.com>
2026-06-21 21:07:54 -0700

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,22 @@
+const cards = [
+  ["hearts", "diamonds", "clubs", "spades"],
+  [
+    "ace",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "jack",
+    "queen",
+    "king"
+  ]
+];
+
+const suits = cards[0];
+const cardNames = cards[1];
+
